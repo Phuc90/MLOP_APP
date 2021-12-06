28fd4458d3137bd5e3c49ebaad2ea328c5d85f5e
Phuc90 <thienphuc225@gmail.com>
2021-12-07 02:30:37 +0700

    setup done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version="0.0.1",
+    description="its a wine Q package",
+    author="Phuc90",
+    packages=find_packages(),
+    license="MIT")
---
+++ tests/test_config.py
@@ -0,0 +1,6 @@
+import pytest
+
+def test_generic():
+    a = 2
+    b = 2
+    assert a == b
